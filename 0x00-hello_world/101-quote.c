f71e8764c842bd4eec552038b195d17bfd03b05e
engysaleh <engysaleh890@gmail.com>
2023-03-03 14:00:53 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - this is the main function
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

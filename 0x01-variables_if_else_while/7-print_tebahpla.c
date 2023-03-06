bca6b5fcc20695104654fd34fd4bae6bda779c08
engysaleh <engysaleh890@gmail.com>
2023-03-06 14:58:54 -0800

    print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -6,18 +6,14 @@
  * Description: print a-z in reverse
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	char ch = 'z';
-
-	while (ch >= 'a')
-	{
-		putchar(ch);
-		--ch;
-	}
+	char x;
+	
+	for (x = 'z' ; x >= 'a' ; x--);
+		puchar(x);	
 	putchar('\n');
-
 	return (0);
 }

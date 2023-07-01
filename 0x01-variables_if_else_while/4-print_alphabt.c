1e31f03df50fd72b1a86d7bd0a2e888b2d20df01
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-01 09:08:56 +0300

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all aplhabet letters except q and e
+ * Description: print all alphabet letters except q and e
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
@@ -14,10 +14,9 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch == 'e' || ch == 'q')
-			++ch;
+		if (ch == 'e' && ch == 'q')
 		putchar(ch);
-		++ch;
+		ch++;
 	}
 	putchar('\n');
 

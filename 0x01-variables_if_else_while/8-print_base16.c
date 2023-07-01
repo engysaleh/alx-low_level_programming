9e9e8e2e45cd7e31fc8ec53eab21191e127cc0ad
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-01 10:30:18 +0300

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@
 int main(void)
 {
 	int i;
-	
-	for(i=0 ; i<=9 ; i++)
+
+	for(i = 0 ; i <= 9 ; i++)
 		putchar(i + '0');
-	for(i=0 ; i<=5 ; i++)
+	for(i = 0 ; i <= 5 ; i++)
 		putchar(i + 'a');
-	
+
 	putchar('\n');
 	return (0);
 }

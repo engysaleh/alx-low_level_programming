efb0f0065f986f71ba54e29d619eba6ec0f07226
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-01 09:19:05 +0300

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: print the value of n status:
- *              greater than, is zero and is not less than 6.
+ * Description: print the value of n status
  *
  * Return: Always O (Success)
  */

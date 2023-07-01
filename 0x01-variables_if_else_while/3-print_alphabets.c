929eed081674ceaed32b6297e81857541d10bb0e
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-01 09:01:25 +0300

    files

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,25 +6,23 @@
  * Description: print alphabet in lowercase then in uppercase
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
 	char ch = 'a';
 	char CH = 'A';
 
-	/*prints a - z*/
 	while (ch <= 'z')
 	{
 		putchar(ch);
-		++ch;
+		ch++;
 	}
 
-	/*print A - Z*/
 	while (CH <= 'Z')
 	{
 		putchar(CH);
-		++CH;
+		CH++;
 	}
 	putchar('\n');
 

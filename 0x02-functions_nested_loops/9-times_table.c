8641a35fecf75eb417b0ba33d07999396ba93bf5
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-03 00:25:33 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,34 @@
 
 void times_table(void)
 {
-	int x, y, z;
+	int i, j, k;
 
-	for (x = 0 ; x <= 9 ; x++)
-	{
-		_putchar(0);
-
-		for (y = 0 ; y <= 9 ; y++)
+		for (i = 0 ; i < 10 ; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			for (j = 0 ; j < 10 ; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
 
-			z = x * y;
 
-			if (z <= 9)
-				_putchar(' ');
-			else
-				_putchar((z / 10) + '0');
-			_putchar((z % 10) + '0');
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} 
+				else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}

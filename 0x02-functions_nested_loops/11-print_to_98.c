12c0b5d1fa9e92b8be091aa94549d511ce5ea929
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-03 02:44:31 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,29 +12,26 @@
 void print_to_98(int n)
 {
 	int i;
-
 	if (n <= 98)
 	{
-		for (i = n ; i <= 98 ; i++)
-		{
-        	printf("%d", i);
-
+        for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
 		if (i != 98)
 		{
-		printf(", ");
+                printf(", ");
 		}
-        	}
-    	}
+        }
+	}
 	else
 	{
-		for (i = n ; i >= 98 ; i--)
+		for (i = n; i >= 98; i--)
 		{
-		printf("%d", i);
-
-		if (i != 98)
-		{
-		printf(", ");
-		}
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	printf("\n");

0872b4523da7eff7e61a6277d4fc741c3db6d2ad
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-02 18:36:39 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with puts function
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {

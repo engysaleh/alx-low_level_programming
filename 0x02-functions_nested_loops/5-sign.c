c739738ae64c07d37d35125218310fa5904aa498
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-02 19:52:26 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
- * print_sign - checks number
- * @n: entero recibo por main.
- * Return: 1 if c is lowercase
+ * print_sign - Entry point
+ *
+ * Description: prints the sign of a number
+ * @n: The number to be checked
+ *
+ * Return: 1, 0, -1
  */
+
 int print_sign(int n)
 {
 	if (n >= 1)

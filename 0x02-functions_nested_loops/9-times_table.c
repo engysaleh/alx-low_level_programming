8abf5da5f4b98070e3bf0b018e4e8d44a9f59f1b
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-03 00:20:27 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,14 +14,15 @@ void times_table(void)
 
 	for (x = 0 ; x <= 9 ; x++)
 	{
+		_putchar(0);
+		
 		for (y = 0 ; y <= 9 ; y++)
 		{
-			z = x * y;
-
-			_putchar(z);
 			_putchar(',');
 			_putchar(' ');
-
+			
+			z = x * y;
+			
 			if (z <= 9)
 				_putchar(' ');
 			else

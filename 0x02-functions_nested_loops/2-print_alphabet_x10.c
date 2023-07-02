6830aefedf0459f4ee63d64e6d57e9fa4f3ab8c7
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-02 19:17:04 +0300

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - Entry point
  *
- * Description: prints 10 times the alphabet, in lowercase 
+ * Description: prints 10 times the alphabet, in lowercase
  *
  * Return: void
  */
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 {
 	int line, ch;
 
-	for(line = 0; line <= 9; line++)
+	for (line = 0; line <= 9; line++)
 	{
-		for(ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}

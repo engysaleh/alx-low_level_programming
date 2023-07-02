372225452e3e867779cc0114388262a02943c149
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-02 18:33:47 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -6,10 +6,12 @@
  * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+ -o `echo $CFILE | cut -d. -f1`.s
+	

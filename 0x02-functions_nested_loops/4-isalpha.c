bc38ece8cb1030aa1bb69b4a65528c4c7dbaa094
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-02 19:47:20 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha - checks letter
- * @c: caracter recibo por main.
- * Return: 1 if c is lowercase
+ * _isalpha - Entry point
+ *
+ * Description: checks for alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }

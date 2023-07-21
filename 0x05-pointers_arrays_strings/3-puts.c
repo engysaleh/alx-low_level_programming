f93117d5de7979b90ba4d6c30231b6b2a2b113dc
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-21 20:35:35 +0300

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _puts - prints a string, followed by a newline to stdout
@@ -6,7 +6,7 @@
  * @str: string parameter to print
  *
  * Return: Nothing
-*/
+ */
 
 void _puts(char *str)
 {

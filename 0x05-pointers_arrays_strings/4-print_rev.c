7840499299745c0e64577fa3450add883eb672dd
engysaleh <122945268+engysaleh@users.noreply.github.com>
2023-07-21 20:56:15 +0300

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@
 
 void print_rev(char *s)
 {
-	int i=0;
+	int i = 0;
 
 	while (s[i])
-		i++;
+			i++;
 
 	while (i--)
-		{
-			_putchar(s[i]);
-		}
-		_putchar('\n');
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
